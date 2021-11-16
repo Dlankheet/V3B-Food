0fd10d9bb5660836c02756c3dfb9496681970694
dlankheet <daniel.lankheet@hotmail.com>
2021-11-16 19:24:41 +0100

    Mongo Demo still not working

---
+++ mongoDemo/src/main/java/com/hu/mongodemo/domain/MessageInput.java
@@ -0,0 +1,14 @@
+package com.hu.mongodemo.domain;
+
+public class MessageInput {
+    private String message;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
+
