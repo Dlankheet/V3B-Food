b82eabe8ca2c89736b073e96ed37a28dbfebf990
Ruben <gitgpg@rubend.nl>
2021-11-23 15:35:47 +0100

    add some comments to the init script of mongo.

--- development/mongo-init.js
+++ development/mongo-init.js
@@ -1,2 +1,3 @@
-db = db.getSiblingDB('ingredients')
-db.ingredients.insert({"name":"javasoup"})
+ingredientsDb = db.getSiblingDB('ingredients')
+ingredientsDb.ingredients.insert({"name":"javasoup"}) //creates a new table with "javasoup" data. this is inserted into the ingredients database.
+//looks like its not possible to create a database or table without inserting data. TODO: research this, and/or move this to the ingredients service.
