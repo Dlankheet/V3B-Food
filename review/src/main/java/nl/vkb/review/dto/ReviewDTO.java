0fccf8020892503f3fc1ffbf9952efebdbce0621
GianniG <xlightningclawx110402@gmail.com>
2021-12-10 10:11:51 +0100

    toevoeging DTO

---
+++ review/src/main/java/nl/vkb/review/dto/ReviewDTO.java
@@ -0,0 +1,14 @@
+package nl.vkb.review.dto;
+
+import nl.vkb.review.Rating;
+
+import java.util.List;
+import java.util.UUID;
+
+public class ReviewDTO {
+	public UUID id;
+	public String description;
+	public List<String> pros;
+	public List<String> cons;
+	public Rating rating;
+}
