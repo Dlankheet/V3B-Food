5ed7f2ce19bd1b10689bdf4767f0d7e2480b1936
GianniG <xlightningclawx110402@gmail.com>
2021-12-10 10:14:46 +0100

    @Id toegevoegd aan Rating klasse

--- review/src/main/java/nl/vkb/review/Rating.java
+++ review/src/main/java/nl/vkb/review/Rating.java
@@ -1,11 +1,13 @@
 package nl.vkb.review;
 
 import lombok.Data;
+import org.springframework.data.annotation.Id;
 
 import java.util.UUID;
 
 @Data
 public class Rating {
+	@Id
 	private UUID id;
 	private double rating;
 }
