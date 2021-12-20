0058bb7022bc0a8c09170e3c46e84a42f3dc85ad
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2021-12-20 11:59:04 +0100

    renamed exchange

--- customer/src/main/java/com/hu/bep3/vkb5/customer/core/port/persistence/AddressRepository.java
+++
@@ -1,9 +0,0 @@
-package com.hu.bep3.vkb5.customer.core.port.persistence;
-
-import com.hu.bep3.vkb5.customer.core.domain.model.Address;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import java.util.UUID;
-
-public interface AddressRepository extends MongoRepository<Address, UUID> {
-}
