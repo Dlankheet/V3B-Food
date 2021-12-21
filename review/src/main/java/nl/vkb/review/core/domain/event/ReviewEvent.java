2b55fe1393c1abf16cea481023f95ac880585c29
GianniG <xlightningclawx110402@gmail.com>
2021-12-21 11:49:12 +0100

    allargsconstructor toegevoegd

--- review/src/main/java/nl/vkb/review/core/domain/event/ReviewEvent.java
+++ review/src/main/java/nl/vkb/review/core/domain/event/ReviewEvent.java
@@ -1,16 +1,16 @@
 package nl.vkb.review.core.domain.event;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.Instant;
 import java.util.UUID;
 
 @Getter
+@AllArgsConstructor
 public abstract class ReviewEvent {
 	public final UUID eventId = UUID.randomUUID();
 	public final Instant eventDate = Instant.now();
 
 	public abstract String getEventKey();
-
-
 }
