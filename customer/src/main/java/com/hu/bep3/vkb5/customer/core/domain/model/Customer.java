a54b275f5abc09ef4f645df316fa54719c857b8a
MarlonScholten <57098905+MarlonScholten@users.noreply.github.com>
2021-12-22 10:19:10 +0100

    code smell for validateEmail and added some comments to Customer class

--- customer/src/main/java/com/hu/bep3/vkb5/customer/core/domain/model/Customer.java
+++ customer/src/main/java/com/hu/bep3/vkb5/customer/core/domain/model/Customer.java
@@ -34,55 +34,95 @@ public class Customer {
 	@Getter(AccessLevel.NONE)
 	private List<CustomerEvent> events = new ArrayList<>();
 
-	public Customer(String firstName, String lastName, String email) {
+	/**
+	 * Constructs the Customer, throws an exception if the given email is invalid
+	 * @param firstName
+	 * @param lastName
+	 * @param email
+	 * @exception InvalidEmailException
+	 */
+	public Customer(String firstName, String lastName, String email) throws InvalidEmailException {
 		this.id = UUID.randomUUID();
 		this.firstName = firstName;
 		this.lastName = lastName;
-		if(this.validateEmail(email)){this.email = email;}
+		this.validateEmail(email);
+		this.email = email;
 		this.addresses = new HashSet<>();
 		this.orderHistory = new HashSet<>();
 		this.reviews = new HashSet<>();
 	}
 
+	/**
+	 * Add an Order ID to our Order history
+	 * @param orderId
+	 */
 	public void orderFood(UUID orderId){
 		orderHistory.add(orderId);
 	}
 
+	/**
+	 * Add a Review ID to our list of Reviews
+	 * @param reviewId
+	 */
 	public void reviewOrder(UUID reviewId) {
 		reviews.add(reviewId);
 	}
 
+	/**
+	 * Change the existing email after validating the new email
+	 * @param newEmail
+	 * @exception InvalidEmailException
+	 */
 	public void changeEmail(String newEmail){
-		if(this.validateEmail(newEmail)){
-			this.email = newEmail;
-		}
+		this.validateEmail(newEmail);
+		this.email = newEmail;
 		this.events.add(new EmailChanged(id, newEmail));
 	}
 
-	public void addAddress(Address address){
+	/**
+	 * Attempt to add an address to the list of Addresses
+	 * @param address
+	 * @exception AddressAlreadyBoundException
+	 */
+	public void addAddress(Address address) throws AddressAlreadyBoundException{
 		boolean added = this.addresses.add(address);
 		if(!added){
 			throw new AddressAlreadyBoundException();
 		}
 	}
 
+	/**
+	 * Remove an Address from the Address list
+	 * @param address
+	 */
 	public void removeAddress(Address address){
 		this.addresses.remove(address);
 	}
 
-	private boolean validateEmail(String email) throws InvalidEmailException {
+	/**
+	 * Validate an Email(String) by checking it against a given regex pattern
+	 * @param email
+	 * @throws InvalidEmailException
+	 */
+	private void validateEmail(String email) throws InvalidEmailException {
 		String regexPattern = "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@"
 				+ "[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-		if(Utils.patternMatches(email, regexPattern)){
-			return true;
+		if(!Utils.patternMatches(email, regexPattern)){
+			throw new InvalidEmailException();
 		}
-		throw new InvalidEmailException();
 	}
 
+	/**
+	 * Clear(remove all) the event list of this class
+	 */
 	public void clearEvents() {
 		this.events.clear();
 	}
 
+	/**
+	 * Retrieve the list of events bound to this class
+	 * @return a list of events
+	 */
 	public List<CustomerEvent> listEvents() {
 		return events;
 	}
