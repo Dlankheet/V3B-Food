58f96bfa1ae3f3cb8241f3ffcbf19d60d6b7767c
GianniG <xlightningclawx110402@gmail.com>
2021-12-23 10:59:38 +0100

    change and create now both have exceptions

--- review/src/main/java/nl/vkb/review/core/domain/Rating.java
+++ review/src/main/java/nl/vkb/review/core/domain/Rating.java
@@ -11,12 +11,16 @@ public class Rating {
 	private double ratingNumber;
 
 	public Rating(double ratingNumber) {
-		if (ratingNumber > 0 && ratingNumber <= 5) this.ratingNumber = ratingNumber;
-		else throw new ReviewRatingException("Could not be created");
+		this.ratingCheck(ratingNumber);
 	}
 
 	public void changeRating(double rNumber) {
-		this.ratingNumber = rNumber;
+		ratingCheck(rNumber);
+	}
+
+	private void ratingCheck(double ratingNumber) {
+		if (ratingNumber > 0 && ratingNumber <= 5) this.ratingNumber = ratingNumber;
+		else throw new ReviewRatingException("Number not within 0 or 5");
 	}
 
 }
