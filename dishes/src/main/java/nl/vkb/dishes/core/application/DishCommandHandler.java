9de32f11b2a8e2fdf91fb2e8acae80a9ca0cd5c0
dlankheet <daniel.lankheet@hotmail.com>
2021-12-23 17:13:49 +0100

    Added 2 functions(add and remove ingredient)
    Took 33 minutes

--- dishes/src/main/java/nl/vkb/dishes/core/application/DishCommandHandler.java
+++ dishes/src/main/java/nl/vkb/dishes/core/application/DishCommandHandler.java
@@ -1,9 +1,12 @@
 package nl.vkb.dishes.core.application;
 
+import nl.vkb.dishes.core.application.command.AddIngredient;
 import nl.vkb.dishes.core.application.command.CreateDish;
 import nl.vkb.dishes.core.application.command.DeleteDish;
+import nl.vkb.dishes.core.application.command.RemoveIngredient;
 import nl.vkb.dishes.core.domain.Dish;
 import nl.vkb.dishes.core.domain.DishRepository;
+import nl.vkb.dishes.core.domain.Exception.DishNotFoundException;
 import nl.vkb.dishes.core.domain.event.DishEvent;
 import nl.vkb.dishes.core.port.messaging.DishEventPublisher;
 import org.springframework.stereotype.Service;
@@ -29,6 +32,17 @@ public class DishCommandHandler {
         this.dishRepo.deleteById(command.getId());
     }
 
+    public Dish handle (AddIngredient command){
+        Dish dish = dishRepo.findById(command.getDishId()).orElseThrow(()-> new DishNotFoundException("Dish does not exist."));
+        dish.addIngredient(command.getIngredient());
+        return this.publishEventsAndSave(dish);
+    }
+
+    public Dish handle (RemoveIngredient command){
+        Dish dish = dishRepo.findById(command.getDishId()).orElseThrow(()-> new DishNotFoundException("Dish does not exist."));
+        dish.removeIngredient(command.getIngredientID());
+        return this.publishEventsAndSave(dish);
+    }
 
     private Dish publishEventsAndSave(Dish dish) {
         List<DishEvent> events = dish.getEvents();
--- dishes/src/main/java/nl/vkb/dishes/infrastructure/driver/web/DishController.java
+++ dishes/src/main/java/nl/vkb/dishes/infrastructure/driver/web/DishController.java
@@ -2,19 +2,21 @@ package nl.vkb.dishes.infrastructure.driver.web;
 
 import nl.vkb.dishes.core.application.DishCommandHandler;
 import nl.vkb.dishes.core.application.DishQueryHandler;
+import nl.vkb.dishes.core.application.command.AddIngredient;
 import nl.vkb.dishes.core.application.command.CreateDish;
 import nl.vkb.dishes.core.application.command.DeleteDish;
+import nl.vkb.dishes.core.application.command.RemoveIngredient;
 import nl.vkb.dishes.core.application.query.CheckOrderAvailability;
 import nl.vkb.dishes.core.application.query.ListDishes;
 import nl.vkb.dishes.core.application.query.CheckAvailable;
 import nl.vkb.dishes.core.application.results.OrderAvailableResult;
 import nl.vkb.dishes.core.domain.Dish;
+import nl.vkb.dishes.core.domain.Ingredient;
 import nl.vkb.dishes.infrastructure.driver.web.request.CreateDishRequest;
 import nl.vkb.dishes.infrastructure.driver.web.result.WebOrderAvailableResult;
 import nl.vkb.dishes.utils.uuidUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -43,13 +45,19 @@ public class DishController {
     @GetMapping("/checkorderavailability/{idString}")
     public WebOrderAvailableResult checkOrderAvailability(@PathVariable String idString) {
         List<UUID> dishUUIDs = uuidUtils.parseStringToList(idString);
-        System.out.println(dishUUIDs);
         OrderAvailableResult result = this.queryHandler.handle(new CheckOrderAvailability(dishUUIDs));
         List<String> resultlist = uuidUtils.parseUUIDtoList(result.getUnavailableDishes());
-
         return new WebOrderAvailableResult(result.allAvailable, resultlist, result.getTotalPrice());
     }
 
+    @PatchMapping("/{dishId}/addIngredient")
+    public Dish addIngredientToDish(@PathVariable UUID dishId, @RequestParam Ingredient ingredient){
+        return this.commandHandler.handle(new AddIngredient(dishId, ingredient));
+    }
+    @PatchMapping("/{dishId}/removeIngredient")
+    public Dish removeIngredientFromDish(@PathVariable UUID dishId, @RequestParam UUID ingredientId){
+        return this.commandHandler.handle(new RemoveIngredient(dishId, ingredientId));
+    }
     @DeleteMapping("/remove")
     public void generateRandomDish(String id) {
         commandHandler.handle(new DeleteDish(UUID.fromString(id)));
