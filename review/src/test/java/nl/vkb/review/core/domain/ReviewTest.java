7afc5d640813133dd5166bb07dbb243a4aa872a6
GianniG <xlightningclawx110402@gmail.com>
2021-12-23 11:02:55 +0100

    2 testen toegevoegd voor changeReview

--- review/src/test/java/nl/vkb/review/core/domain/ReviewTest.java
+++ review/src/test/java/nl/vkb/review/core/domain/ReviewTest.java
@@ -11,35 +11,51 @@ import java.util.UUID;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ReviewTest {
-		private Review review;
-		private Rating rating;
-		private List<String> pros = new ArrayList<>();
-		private List<String> cons = new ArrayList<>();
+	private Review review;
+	private Rating rating;
+	private List<String> pros = new ArrayList<>();
+	private List<String> cons = new ArrayList<>();
 
-		@BeforeEach
-		void init() {
-			pros.add("Good");
-			cons.add("Bad");
-			rating = new Rating(3.4);
-			this.review = new Review("test", pros, cons, rating,
-					UUID.randomUUID(), UUID.randomUUID());
-		}
-		@Test
-		void createReviewTest() {
-			assertNotNull(review.getId());
-		}
+	@BeforeEach
+	void init() {
+		pros.add("Good");
+		cons.add("Bad");
+		rating = new Rating(3.4);
+		this.review = new Review("test", pros, cons, rating,
+				UUID.randomUUID(), UUID.randomUUID());
+	}
+
+	@Test
+	void createReviewTest() {
+		assertNotNull(review.getId());
+	}
 
 	@Test
 	void changeReviewRatingTest() {
-			this.review.getRating().changeRating(4.6);
+		this.review.getRating().changeRating(4.6);
 		assertEquals(4.6, this.review.getRating().getRatingNumber());
 	}
-		@Test
-		void RatingNotValidTest() {
-			assertThrows(ReviewRatingException.class,
-					()-> new Review("d", pros, cons,
-							new Rating(5.1), UUID.randomUUID(), UUID.randomUUID())
-					);
-		}
+
+	@Test
+	void changeReviewRatingFailTest() {
+		assertThrows(ReviewRatingException.class,
+				() -> this.review.getRating().changeRating(5.1)
+		);
 	}
+
+	@Test
+	void changeReviewRatingFailTest2() {
+		assertThrows(ReviewRatingException.class,
+				() -> this.review.getRating().changeRating(0)
+		);
+	}
+
+	@Test
+	void RatingNotValidTest() {
+		assertThrows(ReviewRatingException.class,
+				() -> new Review("d", pros, cons,
+						new Rating(5.1), UUID.randomUUID(), UUID.randomUUID())
+		);
+	}
+}
 
