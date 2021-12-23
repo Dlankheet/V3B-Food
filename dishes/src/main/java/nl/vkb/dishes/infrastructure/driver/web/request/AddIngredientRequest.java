aa2b53ce581175bb3764571643e21fbca66d8fda
Ruben <gitgpg@rubend.nl>
2021-12-23 17:46:58 +0100

    fix code smell.

--- dishes/src/main/java/nl/vkb/dishes/infrastructure/driver/web/request/AddIngredientRequest.java
+++ dishes/src/main/java/nl/vkb/dishes/infrastructure/driver/web/request/AddIngredientRequest.java
@@ -2,9 +2,7 @@ package nl.vkb.dishes.infrastructure.driver.web.request;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import nl.vkb.dishes.core.domain.Ingredient;
 
-import java.util.List;
 import java.util.UUID;
 
 @AllArgsConstructor
